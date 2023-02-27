7b3bffb16e86335d2a404fc05b2c960b653cd58e
sromano87 <siromano@unisa.it>
2023-02-27 11:36:04 +0100

    Modified main with a call to printHelloWorld

--- src/main/java/it/unisa/TestClass.java
+++ src/main/java/it/unisa/TestClass.java
@@ -4,8 +4,12 @@ public class TestClass {
 	
     public static void main (String[] args) {
 		
-    	System.out.println("Hello");
+    	printHelloWorld();
     	
 	}
+    
+    public static void printHelloWorld() {
+    	System.out.println("Hello World!");
+    }
 
 }
